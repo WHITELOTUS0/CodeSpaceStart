3ef1a952bbc8562d8d96664d87f6b18d20eec33c
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-08-30 08:10:19 +0000

    home and login

--- starterapp/src/App.js
+++ starterapp/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={LoginPage} />
+        <PrivateRoute path="/home" component={Home} />
+      </div>
+    </Router>
+  );
+}
+
+// Create a custom route to handle authentication
+function PrivateRoute({ component: Component, ...rest }) {
+  const isAuthenticated = /* Add your authentication logic here */ true; // Replace with your authentication check
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/" />
+        )
+      }
+    />
   );
 }
 
---
+++ starterapp/src/components/LoginPage.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    // Add your authentication logic here (e.g., check username and password)
+    // For this example, we'll just check if both fields are filled
+    if (username && password) {
+      setLoggedIn(true);
+    }
+  };
+
+  if (loggedIn) {
+    // Redirect to the home page after successful login
+    return <Redirect to="/home" />;
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default LoginPage;
