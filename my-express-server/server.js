51ea3afbad47519957cfe34a606c8716f51c12db
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-10-02 11:48:30 +0300

    Update server.js

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -5,4 +5,8 @@ const mongoose = require('mongoose');
 const app=express();
 const port=3000;
 
-mongoose.connect('mongodb://localhost:27017')
+mongoose.connect('mongodb://localhost:27017/centecontactsusers', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+});
